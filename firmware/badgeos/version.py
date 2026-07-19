ca573e5a15aa54134a525caa1b6a26d15b11f94e
3ldr1tch <EldritchMolder@proton.me>
2026-07-19 15:15:08 -0500

    Create version.py

---
+++ firmware/badgeos/version.py
@@ -0,0 +1,13 @@
+"""
+BadgeOS version information.
+"""
+
+VERSION = "0.1.0-dev"
+
+CODENAME = "Sock Puppet"
+
+AUTHOR = "BadgeOS Project"
+
+LICENSE = "MIT"
+
+TARGET = "HackNWA SockPuppetTycoon Badge"
