74fbc378649acb7034a2d481115db51629a09e18
3ldr1tch <EldritchMolder@proton.me>
2026-07-28 09:13:31 -0500

    Update __init__.py

--- firmware/badgeos/core/__init__.py
+++ firmware/badgeos/core/__init__.py
@@ -4,10 +4,8 @@ BadgeOS Core
 
 from .scheduler import Scheduler
 from .service import Service
-from .heartbeat import HeartbeatService
 
 __all__ = [
     "Scheduler",
     "Service",
-    "HeartbeatService",
 ]
